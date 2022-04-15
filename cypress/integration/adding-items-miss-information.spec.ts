4f7bb18507043e070ec8eeea8cc27805cab99a93
jusemore <jusemore@bancolombia.com.co>
2022-04-15 01:47:03 -0500

    creating adding-items-miss-information.spec.ts test

--- cypress/integration/adding-items-miss-information.spec.ts
+++ cypress/integration/adding-items-miss-information.spec.ts
@@ -1,5 +1,22 @@
-describe("Adding items with miss information", () => {
-  it("then the item can't be created", () => {
+import {Item, Types} from "src/interfaces/item";
+import {ItemsListPage} from "../page/items-list.page";
 
+describe("Adding items with miss information", () => {
+  const item: Item = {
+    id: 0,
+    name: "Wine",
+    sellIn: 30,
+    quality: -3,
+    type: Types.AGED
+  };
+  const qualityErrorExpected = "Expected value between 0 and 80";
+  const confirmButtonStateExpected = "be.disabled";
+
+  it("then the item can't be created", () => {
+    ItemsListPage.visit();
+    ItemsListPage.openAddItemDialog()
+    ItemsListPage.filloutItemAttributes(item);
+    ItemsListPage.validateConfirmAddButtonIs(confirmButtonStateExpected);
+    ItemsListPage.validateQualityErrorMessage(qualityErrorExpected)
   });
 })
