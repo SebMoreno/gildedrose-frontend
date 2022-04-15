1dc3fa0e670c6e372abf304d19a35a10c321b21c
jusemore <jusemore@bancolombia.com.co>
2022-04-15 01:46:36 -0500

    Making adding-items.spec.ts idempotent

--- cypress/integration/adding-items.spec.ts
+++ cypress/integration/adding-items.spec.ts
@@ -1,31 +1,38 @@
-import { Item, Types } from "src/interfaces/item";
+import {Item, Types} from "src/interfaces/item";
+import {ItemsListPage} from "../page/items-list.page";
 
 describe("Adding items", () => {
-  it("then a new item should be displayed", () => {
+  const item: Item = {
+    id: 0,
+    name: "Carrot",
+    sellIn: 30,
+    quality: 30,
+    type: Types.LEGENDARY
+  };
 
-    const item: Item = {
-      id: 0,
-      name: "Carrot",
-      sellIn: 30,
-      quality: 30,
-      type: Types.LEGENDARY
-    };
-    cy.visit("/list");
-    cy.get(".list-add-button").click();
-    cy.get("[data-automation=item-form-name]").type(item.name);
-    cy.get("[data-automation=item-form-sell-in]").type(item.sellIn.toString());
-    cy.get("[data-automation=item-form-quality]").type(item.quality.toString());
-    cy.get("[data-automation=item-form-type]").click();
-    cy.get("mat-option").contains(item.type).click();
-    cy.intercept("GET", "/api/items").as("getItems");
-    cy.get("[data-automation=item-form-confirm-button]").click();
-    cy.wait("@getItems");
-    const createdItem = cy.get(".mat-card > :nth-child(1)").filter(`:contains('${item.name}')`)
-      .siblings(":nth-child(2)").filter(`:contains('${item.sellIn}')`)
-      .siblings(":nth-child(3)").filter(`:contains('${item.quality}')`)
-      .siblings(":nth-child(4)").filter(`:contains('${item.type}')`);
-    createdItem.should("exist");
-    createdItem.siblings().find("[data-automation=list-delete-button]").click();
-    cy.get("[data-automation=delete-dialog-confirm-button]").contains("Delete").click();
+  before(() => {
+    cy.request("GET", "/api/items").its("body")
+      .then((items: Item[]) => {
+        const itemInBd = items.find(it =>
+          it.name === item.name &&
+          it.quality === item.quality &&
+          it.type === item.type &&
+          it.sellIn === item.sellIn
+        );
+        if (itemInBd) {
+          cy.request("DELETE", `/api/items/${itemInBd.id}`)
+        }
+      })
+  })
+
+  it("then a new item should be displayed", () => {
+    const itemsToBeCreated = 1;
+    ItemsListPage.visit();
+    ItemsListPage.setAliasItemsList()
+    ItemsListPage.openAddItemDialog()
+    ItemsListPage.filloutItemAttributes(item);
+    ItemsListPage.confirmItemCreation();
+    ItemsListPage.validateItemsListLengthIncreasedBy(itemsToBeCreated)
+    ItemsListPage.validateItemIsListed(item);
   });
 })
---
+++ cypress/page/items-list.page.ts
@@ -0,0 +1,84 @@
+import {Item} from "../../src/interfaces/item";
+
+export class ItemsListPage {
+  private static readonly addItemButton = ".list-add-button";
+  private static readonly deleteItemButton = "[data-automation=list-delete-button]";
+  private static readonly nameInput = "[data-automation=item-form-name]";
+  private static readonly sellInInput = "[data-automation=item-form-sell-in]";
+  private static readonly qualityInput = "[data-automation=item-form-quality]";
+  private static readonly typeInput = "[data-automation=item-form-type]";
+  private static readonly matOption = "mat-option";
+  private static readonly addItemConfirmButton = "[data-automation=item-form-confirm-button]";
+  private static readonly deleteItemConfirmButton = "[data-automation=delete-dialog-confirm-button]";
+  private static readonly getItemsRequestAlias = "getItems";
+  private static readonly deleteItemRequestAlias = "deleteItem";
+  private static readonly itemsListAlias = "itemsList";
+  private static readonly loadingSpinner = ".list-empty .mat-progress-spinner";
+  private static readonly itemsListRows = "[data-automation=list-item-row]";
+  private static readonly qualityErrorMessage = "[data-automation=item-form-quality] .mat-error";
+
+  public static visit() {
+    cy.intercept("GET", "/api/items").as(this.getItemsRequestAlias);
+    cy.visit("/list");
+    this.waitListToRender(this.getItemsRequestAlias);
+  }
+
+  public static openAddItemDialog() {
+    cy.get(this.addItemButton).click();
+  }
+
+  public static filloutItemAttributes(item: Item) {
+    cy.get(this.nameInput).type(item.name);
+    cy.get(this.sellInInput).type(item.sellIn.toString());
+    cy.get(this.qualityInput).type(item.quality.toString());
+    cy.get(this.typeInput).click();
+    cy.get(this.matOption).contains(item.type).click();
+  }
+
+  public static confirmItemCreation() {
+    cy.intercept("GET", "/api/items").as(this.getItemsRequestAlias);
+    cy.get(this.addItemConfirmButton).click();
+    this.waitListToRender(this.getItemsRequestAlias)
+  }
+
+  public static getItem(item: Item) {
+    return cy.get(".mat-card > :nth-child(1)").filter(`:contains('${item.name}')`)
+      .siblings(":nth-child(2)").filter(`:contains('${item.sellIn}')`)
+      .siblings(":nth-child(3)").filter(`:contains('${item.quality}')`)
+      .siblings(":nth-child(4)").filter(`:contains('${item.type}')`);
+  }
+
+  public static deleteItem(item: Item) {
+    cy.intercept("DELETE", "/api/items/*").as(this.deleteItemRequestAlias);
+    this.getItem(item).siblings().find(this.deleteItemButton).click();
+    cy.get(this.deleteItemConfirmButton).contains("Delete").click();
+    this.waitListToRender(this.deleteItemRequestAlias);
+  }
+
+  public static waitListToRender(requestAlias: string) {
+    cy.wait("@" + requestAlias);
+    cy.get(this.loadingSpinner).should("not.exist")
+  }
+
+  public static validateItemIsListed(item: Item) {
+    this.getItem(item).should("exist")
+  }
+
+  public static setAliasItemsList() {
+    cy.get(this.itemsListRows).should(() => true).as(this.itemsListAlias)
+  }
+
+  public static validateItemsListLengthIncreasedBy(number: number) {
+    cy.get("@" + this.itemsListAlias).then(initialItemsList =>
+      cy.get(this.itemsListRows).its("length")
+        .should("equal", initialItemsList.length + number)
+    );
+  }
+
+  public static validateConfirmAddButtonIs(state: string) {
+    cy.get(this.addItemConfirmButton).should(state);
+  }
+  public static validateQualityErrorMessage(expectedMessage: string) {
+    cy.get(this.qualityErrorMessage).should("contain.text", expectedMessage);
+  }
+}
