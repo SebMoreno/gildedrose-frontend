e15f10c7fc885b14809d9b21dcd68948fedcd595
Sebastián <juansebasmorenoruiz@gmail.com>
2022-04-24 22:04:11 -0500

    fixing proxy error

--- proxy.config.js
+++ proxy.config.js
@@ -1,6 +1,6 @@
 const PROXY_CONFIG = {
   "/api/": {
-    "target" : "http://" + process.env.API_HOST_URL || "http://localhost:8080",
+    "target" : "http://" + (process.env.API_HOST_URL || "localhost:8080"),
     "secure": false,
     "logLevel": "debug"
   }
