5c951a2b466a0ba4342f6968b9d0e5d62d246a20
Sebastián <juansebasmorenoruiz@gmail.com>
2022-05-17 22:35:25 -0500

    terraform with variable values

--- terraform-ci-cd/deploy/main.tf
+++ terraform-ci-cd/deploy/main.tf
@@ -2,19 +2,19 @@ module "deploy_ec2" {
   source        = "../infra"
   aws_region    = "us-east-1"
   instance_type = "t2.small"
-  ec2_tags      = { Name = "grupo2-jenkins-server" }
+  ec2_tags      = { Name = "grupo2-ec2" }
   ami_id        = "ami-005de95e8ff495156"
   key_pair_name = "grupo2"
   user_data     = file("../infra/user_data.sh")
 
-  eip_tags = { Name = "Pacman-demo-terraform-sg" }
+  eip_tags = { Name = "grupo2-eip" }
 
   subnet_id = "subnet-04e972f3a706c00e8"
   public_ip = false
 
   sg_name             = "grupo2-sg"
-  sg_description      = "Allow http over port 80 and ssh over port 22"
+  sg_description      = "Allow http over port 80 from anywhere and other ports for testing"
   vpc_id              = "vpc-031420f7c99b1a0bd"
-  allowed_cidr_for_sg = ""
-  sg_tags             = { Name = "Pacman-demo-terraform-sg" }
+  allowed_cidr_for_sg = "181.51.33.9/32"
+  sg_tags             = { Name = "grupo2-sg" }
 }
